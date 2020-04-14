dca284301148509ecbfa1facc558f7677ed3f3f7
William <william.l.munoz@gmail.com>
2020-04-14 19:14:28 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -8,7 +8,7 @@ end
 
 num_of_hotdogs_eaten = 0
 # => 0 (return value)
- 
+
 while num_of_hotdogs_eaten < 7
   num_of_hotdogs_eaten += 1
   puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
